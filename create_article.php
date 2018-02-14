793f7b027be842b05aae1fa6c0211a0edc7b352d
a010380 <33076994+a010380@users.noreply.github.com>
2018-02-14 09:45:23 +0800

    insert date in MySQL
    test

---
+++ create_article.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML>
+<html>  
+<body>
+
+<form action="/receive_data.php" method="post">
+
+Title: <input type="text" name="title"><br>
+Body: <br>
+  <textarea name="body" style="width:200px; height:200px">
+    The body was filling in this.
+  </textarea>
+<br>
+<input type="submit">
+</form>
+
+</body>
+</html>
