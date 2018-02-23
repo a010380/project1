e2c9eff78281e81c96f3f61198071eab3f614b4d
a010380 <33076994+a010380@users.noreply.github.com>
2018-02-23 09:10:10 +0800

    修改細微bug
    2018/02/23

--- receive_data.php
+++ receive_data.php
@@ -4,22 +4,27 @@ $username = "a010380";
 $password = "414852";
 $dbname = "hw3";
 
+//http://r2internote.blogspot.tw/2013/10/phpmysql.html
+$title =$_REQUEST['title'];
+$body =$_REQUEST['body'];
+
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 // Check connection
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
-
 date_default_timezone_set('Asia/Taipei');
+
 $id=6;
 $datetime = date ("Y- m - d / H : i : s"); 
 
 $sql = "INSERT INTO articles (id, title, body, created_at)
-VALUES ('$id', 'title', 'body','$datetime')";
+VALUES ('$id', '$title', '$body','$datetime')";
 
 if (mysqli_query($conn, $sql)) {
-    echo "New record created successfully";
+  $last_id = mysqli_insert_id($conn);
+    echo "New record created successfully. Last inserted ID is: " . $last_id;
 } else {
     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
 }
